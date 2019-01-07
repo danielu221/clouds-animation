e843e546654e759a3604c6b6fefc92851c2e20b6
Mateusz <mateusz.danieluk@ge.com>
2019-01-07 20:44:14 +0100

    Add cockpit mask

--- clouds.js
+++ clouds.js
@@ -163,8 +163,8 @@ function initClouds() {
 
     position = ((Date.now() - start_time) * 0.03) % 8000;
 
-    camera.position.x += (mouseX - camera.position.x) * 0.01;
-    camera.position.y += (-mouseY - camera.position.y) * 0.01;
+    camera.position.x += (mouseX - camera.position.x) * 0.005;
+    camera.position.y += (-mouseY - camera.position.y) * 0.005;
     camera.position.z = -position + 8000;
 
     renderer.render(scene, camera);
